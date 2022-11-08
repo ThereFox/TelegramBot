784465d942ee7163f5e409450d6ee5fe7fe95cac
ThereFox <alexandrgrizunow@gmail.com>
2022-11-09 02:32:12 +0400

    refactoring view

---
+++ View/Handler.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+using ConsoleApp2.Controllers;
+
+namespace ConsoleApp2.View
+{
+    public class Handler
+    {
+        private ResponseFactory _responseFactory;
+
+        public Handler()
+        {
+            _responseFactory = new ResponseFactory();
+        }
+
+        public async Task GetMessageHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var response = await _responseFactory.getRespons(update.Message);
+
+            ChatId chatId = update.Message.Chat.Id;
+
+            await client.SendTextMessageAsync(chatId, response);
+        }
+        public async Task ErrorHandl(ITelegramBotClient client, Exception exeption, CancellationToken token)
+        {
+            await Task.CompletedTask;
+            Console.WriteLine(exeption.Message.ToString());
+        }
+    }
+}
